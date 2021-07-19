d67dde5239e7f94af8b7a58e7eba2b27ac7e01bc
billy@higtsolutions.com <billy@hightsolutions.com>
2021-07-19 08:30:24 -0500

    Styled networks page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -126,6 +126,7 @@ class App extends React.Component {
 		this.handleCheckChange = this.handleCheckChange.bind(this);
 		this.updateNetworks = this.updateNetworks.bind(this);
 		this.handleNetworksChange = this.handleNetworksChange.bind(this);
+		this.handleRouterDhcpChange = this.handleRouterDhcpChange.bind(this);
 	};
 
 	fetchCompany() {
@@ -703,6 +704,13 @@ class App extends React.Component {
 		});
 	};
 
+	handleRouterDhcpChange(event) {
+		this.setState({
+			routerDhcp: !this.state.routerDhcp
+		});
+	}
+
+
 //React Hooks
 	componentDidMount() {
 		this.fetchAllCompanies();	
@@ -790,7 +798,7 @@ class App extends React.Component {
 					<Navbar changeNav={this.changeNav} />
 					<Topbar companyList={this.state.companyList} fetchAllCompanies={this.fetchAllCompanies} selectCompany={this.selectCompany} companyName={this.state.companyName}/>
 					<div id="content-container">			 		
-						<Networks handleNetworksChange={this.handleNetworksChange} updateNetworks={this.updateNetworks} {...this.state}/>	
+						<Networks handleRouterDhcpChange={this.handleRouterDhcpChange} handleNetworksChange={this.handleNetworksChange} updateNetworks={this.updateNetworks} {...this.state}/>	
 					</div>
 				</div>
 			)
@@ -1184,50 +1192,84 @@ class Networks extends React.Component {
 	render() {
 		return (
 			<div id="network-container">
-				<div id="network-overview-card" className="network-card">
-					<label htmlFor="subnet">Subnet</label>
-					<input name="subnet" type="text" value={this.props.subnet} onChange={this.props.handleNetworksChange} />
-					<label htmlFor="public">Public IP</label>
-					<input name="public" type="text" value={this.props.public} onChange={this.props.handleNetworksChange}/>
-					<label htmlFor="gateway">Gateway Address</label>
-					<input name="gateway" type="text" value={this.props.gateway} onChange={this.props.handleNetworksChange}/>
-					<label htmlFor="dns">DNS Server</label>
-					<input name="dns" type="text" value={this.props.dns} onChange={this.props.handleNetworksChange}/>
-					<label htmlFor="ddns">DDNS Address</label>
-					<input name="ddns" type="text" value={this.props.ddns} onChange={this.props.handleNetworksChange}/>
-					<label htmlFor="dhcpServer">DHCP Server</label>
-					<input name="dhcpServer" type="text" value={this.props.dhcpServer} onChange={this.props.handleNetworksChange}/>
-					<label htmlFor="dhcpScope">DHCP Scope</label>
-					<input name="dhcpScope" type="text" value={this.props.dhcpScope} onChange={this.props.handleNetworksChange}/>
-					<label htmlFor="domainController">Domain Controller</label>
-					<input name="domainController" type="text" value={this.props.domainController} onChange={this.props.handleNetworksChange}/>
+				<div id="network-card-container">
+				
+					<div id="network-overview-card" className="network-card">
+						<h2>Network Overview</h2>
+						<div id="network-fields-container">
+							<div className="network-overview-fields">
+								<label htmlFor="subnet">Subnet</label>
+								<input name="subnet" type="text" value={this.props.subnet} onChange={this.props.handleNetworksChange} />
+								<label htmlFor="public">Public IP</label>
+								<input name="public" type="text" value={this.props.public} onChange={this.props.handleNetworksChange}/>
+								<label htmlFor="gateway">Gateway Address</label>
+								<input name="gateway" type="text" value={this.props.gateway} onChange={this.props.handleNetworksChange}/>
+								<label htmlFor="dns">DNS Server</label>
+								<input name="dns" type="text" value={this.props.dns} onChange={this.props.handleNetworksChange}/>
+							</div>
+							<div className="network-overview-fields">
+								<label htmlFor="ddns">DDNS Address</label>
+								<input name="ddns" type="text" value={this.props.ddns} onChange={this.props.handleNetworksChange}/>
+								<label htmlFor="dhcpServer">DHCP Server</label>
+								<input name="dhcpServer" type="text" value={this.props.dhcpServer} onChange={this.props.handleNetworksChange}/>
+								<label htmlFor="dhcpScope">DHCP Scope</label>
+								<input name="dhcpScope" type="text" value={this.props.dhcpScope} onChange={this.props.handleNetworksChange}/>
+								<label htmlFor="domainController">Domain Controller</label>
+								<input name="domainController" type="text" value={this.props.domainController} onChange={this.props.handleNetworksChange}/>
+							</div>
+						</div>
+					</div>
+					<div id="router-card" className="network-card">
+						<h2>Router</h2>
+						<div id="router-fields-container">
+							<div className="router-fields">
+								<label htmlFor="routerIpAddr">IP Address</label>
+								<input name="routerIpAddr" type="text" value={this.props.routerIpAddr} onChange={this.props.handleNetworksChange}/>
+								<label htmlFor="routerPortForwards">Port Forwards</label>
+								<input name="routerPortForwards" type="text" value={this.props.routerPortForwards} onChange={this.props.handleNetworksChange}/>
+								<label htmlFor="routerDhcp">DHCP</label>
+								<form id="router-switch" className="form-check form-switch">
+									<input name="routerDhcp" id="flexSwitchCheckDefault" className="form-check-input" type="checkbox" checked={this.props.routerDhcp} value={this.props.routerDhcp} onChange={this.props.handleRouterDhcpChange}/>
+								</form>
+							</div>
+							<div className="router-fields">
+								<label htmlFor="routerNotes">Notes</label>
+								<textarea name="routerNotes" type="text" value={this.props.routerNotes} onChange={this.props.handleNetworksChange}/>
+							</div>
+						</div>
+
+					</div>
+					<div id="wireless-card" className="network-card">
+						<h2>Wireless</h2>
+						<label htmlFor="wirelessSsid">SSID</label>
+						<input name="wirelessSsid" type="text" value={this.props.wirelessSsid} onChange={this.props.handleNetworksChange}/>
+						<label htmlFor="wirelessEncryption">Wireless Encryption</label>
+						<input name="wirelessEncryption" type="text" value={this.props.wirelessEncryption} onChange={this.props.handleNetworksChange}/>
+						<label htmlFor="wirelessMgmtURL">Management URL</label>
+						<input name="wirelessMgmtURL" type="text" value={this.props.wirelessMgmtURL} onChange={this.props.handleNetworksChange}/>
+					</div>
+					<div id="vpn-card" className="network-card">
+						<h2>VPN</h2>
+						<label htmlFor="vpnType">VPN Type</label>
+						<input name="vpnType" type="text" value={this.props.vpnType} onChange={this.props.handleNetworksChange}/>
+						<label htmlFor="vpnPskLocation">PSK Location</label>
+						<input name="vpnPskLocation" type="text" value={this.props.vpnPskLocation} onChange={this.props.handleNetworksChange}/>
+						<label htmlFor="vpnPublicIP">VPN Server Address</label>
+						<input name="vpnPublicIP" type="text" value={this.props.vpnPublicIP} onChange={this.props.handleNetworksChange}/>
+						<label htmlFor="vpnClient">VPN Client</label>
+						<input name="vpnClient" type="text" value={this.props.vpnClient} onChange={this.props.handleNetworksChange}/>
+					</div>
 				</div>
-				<button onClick={this.props.updateNetworks}>Save</button>
+
+				<button id="router-save-button" onClick={this.props.updateNetworks}>Save</button>
 			</div>
 		)
 	}
 }
 
 /*
-					dns: res.networks.dns,
-					ddns: res.networks.ddns,
-					subnet: res.networks.subnet,
-					public: res.networks.public,
-					gateway: res.networks.gateway,
-					domainController: res.networks.domainController,
-					dhcpServer: res.networks.dhcpServer,
-					dhcpScope: res.networks.dhcpScope,
-					
-					routerSubnet: res.networks.router.subnet,
-					routerIpAddr: res.networks.router.ipAddr,
-					routerDhcp: res.networks.router.dhcp,
-					routerPortForwards: res.networks.router.portForwards,
-					routerNotes: res.networks.router.notes,
-					routerVpn: res.networks.router.vpn,
-					
-					wirelessSsid: res.networks.wireless.ssid,
-					wirelessEncryption: res.networks.wireless.encryption,
-					wirelessMgmtURL: res.networks.wireless.mgmtURL,
+		
+				
 					
 					vpnType: res.networks.vpn.type,
 					vpnPskLocation: res.networks.vpn.pskLocation,
