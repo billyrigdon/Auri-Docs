182483c6ab1eafb62c8c8b677b59c4cba8f359e0
Billy Rigdon <billyrigdoniii@gmail.com>
2022-02-16 11:20:18 -0600

    Styling changes

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,31 +1,29 @@
-//import '../styles/App.css';
+import "../styles/App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Redirect } from "react-router";
 import { useState, useEffect } from "react";
 import Login from "../Screens/Login";
-import {PrivateRoute} from "./PrivateRoute";
-import Auri from "../Screens/Auri"
-
-
+import { PrivateRoute } from "./PrivateRoute";
+import Auri from "../Screens/Auri";
+import "../styles/styles.css";
 
 //Render App component
 const App = () => {
-
-	const [userInfo,setUserInfo] = useState("");
+	const [userInfo, setUserInfo] = useState("");
 	console.log(userInfo);
-	
-	useEffect(()=> {
+
+	useEffect(() => {
 		setUserInfo(localStorage.getItem("userInfo"));
-	},[]);
-	
+	}, []);
+
 	return (
 		<Router>
 			<PrivateRoute exact path="/" component={Auri} />
 			<Route path="/login">
 				{userInfo ? <Redirect to="/" /> : <Login />}
-			</Route>	
+			</Route>
 		</Router>
 	);
-}
+};
 
 export default App;
